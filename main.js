eaf6ccbe1bffa5296d7c957e3837e6402a32d8ea
Yujin Yano <northwindapps@gmail.com>
2023-11-26 18:38:20 +0900

    text25

--- main.js
+++ main.js
@@ -1,7 +1,30 @@
 // Audio files and their sources
 const audioFiles = [
-    { src: './1-1.m4a', text: 'Ja, dein Rad hab ich abgeholt,' },
-    { src: './1-2.m4a', text: 'ich fahre gerade drauf.' }
+    { src: './audio/1.mp3', text: 'Ja, dein Rad hab ich abgeholt,' },
+    { src: './audio/2.mp3', text: 'ich fahre gerade drauf.' },
+    { src: './audio/3.mp3', text: 'Und du wirst lachen,' },
+    { src: './audio/4.mp3', text: 'das macht richtig Spaß.' },
+    { src: './audio/5.mp3', text: 'Ach, scheiße.' },
+    { src: './audio/6.mp3', text: 'Nein, nicht du,' },
+    { src: './audio/7.mp3', text: 'mir ist die Tüte runtergefallen.' },
+    { src: './audio/8.mp3', text: 'Die Brötchentüte!' },
+    { src: './audio/9.mp3', text: 'Bleib mal dran.' },
+    { src: './audio/10.mp3', text: 'So hier bin ich wieder Hagen.' },
+    { src: './audio/11.mp3', text: 'Hagen?Hallo?' },
+    { src: './audio/12.mp3', text: 'Ja!' },
+    { src: './audio/13.mp3', text: 'Hagen nein,' },
+    { src: './audio/14.mp3', text: 'das ist meine einzige freie Woche.' },
+    { src: './audio/15.mp3', text: 'Ja ich weiß ich bin Kims Patenonkel,' },
+    { src: './audio/16.mp3', text: 'aber sie hasst mich' },
+    { src: './audio/17.mp3', text: 'Tut sie wohl, ' },
+    { src: './audio/18.mp3', text: 'sie hat‘s mir gesagt und zwar mehrmals.' },
+    { src: './audio/19.mp3', text: 'Wann bringst du sie?' },
+    { src: './audio/20.mp3', text: 'Sind Sie verletzt?' },
+    { src: './audio/21.mp3', text: 'Sind Sie wahnsinnig?' },
+    { src: './audio/22.mp3', text: "Auf'm Fahrrad Zeitung lesen,das gibt's doch nicht." },
+    { src: './audio/23.mp3', text: 'Das... He... kann das sein, dass ich Sie aus dem Fernsehen kenne?' },
+    { src: './audio/24.mp3', text: 'Sie sind doch der Lustige!' },
+    { src: './audio/25.mp3', text: 'Nein, ich bin Radprofi.' },
     // Add more audio files as needed
   ];
 
@@ -28,6 +51,12 @@ const audioFiles = [
       // Append elements to the container
       audioButtonsContainer.appendChild(button);
       audioButtonsContainer.appendChild(audioElement);
+      const periodRegex =  /[.!?]/;
+      if (periodRegex.test(audio.text)) {
+        // Create a line break element
+        const lineBreak = document.createElement('br');
+        audioButtonsContainer.appendChild(lineBreak);
+      } 
     });
   }
 
